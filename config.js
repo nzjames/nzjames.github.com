ad8f5e7684c468b8c61ec67267a88156c70a61da
James Magness <jamesm@conqahq.com>
2024-08-01 16:53:51 +1200

    fix the build errors

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ export const siteUrl = (()=> {
 	switch(deploymentEnv) {
 	case 'production': return 'https://nzjames.com';
 	case 'staging': return 'https://com-nzjames-staging.fly.dev/';
+	case 'glitch': return '';
 	default: return 'http://localhost:8080'
 	}
 })();
