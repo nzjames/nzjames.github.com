07171bf2f94b0cfe72d1626d5bd7e08861bf5d36
James Magness <jamesm@conqahq.com>
2024-09-17 17:51:21 +1200

    new rule new post

--- glitch/glitch-crawl.test.js
+++ glitch/glitch-crawl.test.js
@@ -2,23 +2,26 @@
 // invokes in the glitch dir.
 // TODO abstract `_glitch` & `_glitch/screenshot` target strings.
 import { test, expect } from "@playwright/test";
+import { existsSync } from "node:fs";
 
 const host = "http://localhost";
 const port = "8099";
 const glitchPaperUrl = `${host}:${port}/glitch-paper.html`;
 
-const renderAndShoot = async (page, item, area='posts') => {
-		const url = `${host}:${port}${item.pageUrl}`;
-		console.log(url);
-		await page.goto(url);
-		await page.screenshot({ path: `_glitch/screenshot/${item.fileName}.png` });
+const renderAndShoot = async (page, item, area = "posts") => {
+	const url = `${host}:${port}${item.pageUrl}`;
+	console.log(url);
+	await page.goto(url);
+	await page.screenshot({ path: `_glitch/screenshot/${item.fileName}.png` });
 
-		// Update the glitch-paper.html
-		// DONE  to export-svg and add download-button,
-		// DONE to export-png and add a download button.
-		// use playwright to request both.
+	// Update the glitch-paper.html
+	// DONE  to export-svg and add download-button,
+	// DONE to export-png and add a download button.
+	// use playwright to request both.
 
-		const glitchPaperUrlFULL = `${glitchPaperUrl}?img=/screenshot/${item.fileName}.png&fileName=${item.fileName}&title=${item.title}`;
+	const glitchPaperUrlFULL = `${glitchPaperUrl}?img=/screenshot/${item.fileName}.png&fileName=${item.fileName}&title=${item.title}`;
+	const glitchFileName = `_glitch/screenshot/paper-${item.fileName}.png`;
+	if (!existsSync(glitchFileName)) {
 		console.log(glitchPaperUrlFULL);
 
 		await page.goto(glitchPaperUrlFULL);
@@ -28,27 +31,44 @@ const renderAndShoot = async (page, item, area='posts') => {
 			path: `_glitch/screenshot/paper-${item.fileName}.png`,
 		});
 
-		// Start waiting for download before clicking. Note no await.
-		const downloadPromise = page.waitForEvent("download");
-		await page.getByText("Download PNG").click();
-		const download = await downloadPromise;
-		// Wait for the download process to complete and save the downloaded file somewhere.
-		await download.saveAs(`content/${area}/img/banner/` + download.suggestedFilename());
+		const pngFileName = `content/${area}/img/banner/${item.fileName}.png`;
+		const svgFileName = `content/${area}/img/banner/${item.fileName}.svg`;
 
-		// Start waiting for download before clicking. Note no await.
-		const downloadPromiseSVG = page.waitForEvent("download");
-		await page.getByText("Download SVG").click({ timeout: 50000 });
-		const downloadSVG = await downloadPromiseSVG;
-		// Wait for the download process to complete and save the downloaded file somewhere.
-		await downloadSVG.saveAs(`content/${area}/img/banner/` + downloadSVG.suggestedFilename());
+		if (existsSync(pngFileName)) {
+			console.log("HAZ ", pngFileName);
+		} else {
+			// Start waiting for download before clicking. Note no await.
+			const downloadPromise = page.waitForEvent("download");
+			await page.getByText("Download PNG").click();
+			const download = await downloadPromise;
+			// Wait for the download process to complete and save the downloaded file somewhere.
+			await download.saveAs(
+				`content/${area}/img/banner/` + download.suggestedFilename()
+			);
+		}
 
-}
+		if (existsSync(svgFileName)) {
+			console.log("HAZ ", svgFileName);
+		} else {
+			// Start waiting for download before clicking. Note no await.
+			const downloadPromiseSVG = page.waitForEvent("download");
+			await page.getByText("Download SVG").click({ timeout: 50000 });
+			const downloadSVG = await downloadPromiseSVG;
+			// Wait for the download process to complete and save the downloaded file somewhere.
+			await downloadSVG.saveAs(
+				`content/${area}/img/banner/` + downloadSVG.suggestedFilename()
+			);
+		}
+	}
+};
 
+// todo, test if the file is there.
+// 1. identify suggestedFileName...
+// .  it should be the title through the slugify function.
 test("crawl glitch posts", async ({ request, page }) => {
-  test.setTimeout(120000);
+	test.setTimeout(120000);
 	const dataUrl = `${host}:${port}/glitch.json`;
 	const testData = await (await request.get(dataUrl)).json();
-
 
 	for (let i = 0; i < testData.posts.length; i += 1) {
 		const item = testData.posts[i];
@@ -57,16 +77,14 @@ test("crawl glitch posts", async ({ request, page }) => {
 	await renderAndShoot(page, testData.posts[0]);
 });
 
-
 test("crawl glitch thoughts", async ({ request, page }) => {
-  test.setTimeout(120000);
+	test.setTimeout(120000);
 	const dataUrl = `${host}:${port}/glitch.json`;
 	const testData = await (await request.get(dataUrl)).json();
 
-
 	for (let i = 0; i < testData.thoughts.length; i += 1) {
 		const item = testData.thoughts[i];
-		await renderAndShoot(page, item, 'thoughts');
+		await renderAndShoot(page, item, "thoughts");
 	}
-	await renderAndShoot(page, testData.thoughts[0], 'thoughts');
+	await renderAndShoot(page, testData.thoughts[0], "thoughts");
 });
